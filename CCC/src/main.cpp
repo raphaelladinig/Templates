4dbf23f368eaf9eb8c03f2e9b9e635c9ec3f9daf
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-03-31 09:13:22 +0200

    update templates

--- CCC/src/main.cpp
+++ CCC/src/main.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 const int level = 0;
--- Codeforces/src/main.cpp
+++ Codeforces/src/main.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+void solve(){
+
+}
+
 int main() {
-    int t;
-    cin >> t;
-    
-    for (int i = 0; i < t; t++) {
-
-    }
-
+    ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    int t; cin >> t; while(t--) {solve();}
     return 0;
 }
